baba2196356a1ad805c021352c415aa4814ca12a
Adrian <adrianhartanto0312@gmail.com>
2024-02-15 13:19:35 +0700

    Feat: add remove task capabilities

--- todo_list/README.md
+++ todo_list/README.md
@@ -1,3 +1,3 @@
 # Todo List Application 📃
 
-This is a program is a todo list application, where so far, the user can only do two main operations. Mainly, adding new tasks and viewing all tasks.
+This is a program is a todo list application, where so far, the user can only do three main operations. Mainly, adding new tasks, removing existing tasks, and viewing all tasks.
--- todo_list/src/main.rs
+++ todo_list/src/main.rs
@@ -4,12 +4,15 @@ use std::io::Write;
 fn handle_command_input() -> u8 {
     println!("1. View all tasks");
     println!("2. Add new task");
-    println!("3. Exit");
+    println!("3. Remove a task");
+    println!("4. Exit");
     print!(">> ");
     let _ = io::stdout().flush();
-    
+
     let mut command_input = String::new();
-    io::stdin().read_line(&mut command_input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut command_input)
+        .expect("Failed to read line");
     let command_input: u8 = command_input.trim().parse().expect("Not a number!");
 
     command_input
@@ -27,13 +30,7 @@ fn view_tasks(task_list: &Vec<String>) {
             println!("|  {0: <2} | {1: <26}|", index + 1, task);
         }
     }
-
     println!("===================================");
-    println!("Press enter to continue...");
-
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Failed to read line!");
-    println!("\n\n");
 }
 
 fn add_task(task_list: &mut Vec<String>) {
@@ -41,7 +38,9 @@ fn add_task(task_list: &mut Vec<String>) {
     loop {
         print!("Task Name: ");
         let _ = io::stdout().flush();
-        io::stdin().read_line(&mut new_task_name).expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut new_task_name)
+            .expect("Failed to read line!");
         new_task_name = new_task_name.trim().to_string();
         if new_task_name.len() > 0 {
             break;
@@ -51,6 +50,27 @@ fn add_task(task_list: &mut Vec<String>) {
     }
     let new_task_name = new_task_name.trim().to_string();
     task_list.push(new_task_name);
+    println!("Update: Successfully added new task!");
+}
+
+fn remove_task(task_list: &mut Vec<String>) {
+    if task_list.len() == 0 {
+        println!("You don't have any tasks at the moment");
+        return;
+    }
+
+    let mut remove_task_index = String::new();
+    view_tasks(task_list);
+    println!("Which task would you like to remove?");
+    print!(">> ");
+    let _ = io::stdout().flush();
+    io::stdin()
+        .read_line(&mut remove_task_index)
+        .expect("Failed to read line!");
+
+    let remove_task_index: usize = remove_task_index.trim().parse().expect("Not a number!");
+    task_list.remove(remove_task_index - 1);
+    println!("Update: Successfully removed task!");
 }
 
 fn main() {
@@ -62,14 +82,23 @@ fn main() {
         match command {
             1 => {
                 view_tasks(&task_list);
-            },
+            }
             2 => {
                 add_task(&mut task_list);
-            },
+            }
             3 => {
+                remove_task(&mut task_list);
+            }
+            4 => {
                 break;
-            },
+            }
             _ => {}
         };
+        println!("Press enter to continue...");
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line!");
+        println!("\n\n\n\n");
     }
 }
