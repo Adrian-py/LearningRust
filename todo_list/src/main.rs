a566546d8cceadd565cb57098fd00604ae85bec8
Adrian <adrianhartanto0312@gmail.com>
2024-02-17 22:43:51 +0700

    Feat: add clear screen feature for better terminal output and add validation to adding tasks

--- todo_list/src/main.rs
+++ todo_list/src/main.rs
@@ -10,15 +10,21 @@ enum Command {
 }
 
 impl Command {
+    // To add more commands, add another case here
     fn from(input: u8) -> Command {
         match input {
             1 => Command::View,
             2 => Command::Add,
             3 => Command::Remove,
-            4 => Command:: Exit,
-            _ => Command:: Unknown, 
+            4 => Command::Exit,
+            _ => Command::Unknown,
         }
     }
+}
+
+fn clear_screen() {
+    // Clears screen and moves cursor to top left of terminal screen
+    print!("\x1B[2J\x1B[1;1H");
 }
 
 fn handle_command_input() -> Command {
@@ -28,27 +34,27 @@ fn handle_command_input() -> Command {
     println!("4. Exit");
     print!(">> ");
     let _ = io::stdout().flush();
+
     let mut command_input = String::new();
     io::stdin()
         .read_line(&mut command_input)
         .expect("Failed to read line");
-    let command_input: Command = match command_input.trim().parse() {
-        Ok(com) => {
-            Command::from(com)
-        },
+    let command_input: Command = match command_input.trim().parse::<u8>() {
+        Ok(com) => Command::from(com),
         Err(_) => {
             println!("Please enter a command number!");
             Command::Unknown
         }
     };
+
     command_input
 }
 
 fn view_tasks(task_list: &Vec<String>) {
+    println!("Your Tasks");
     println!("===================================");
     println!("| No. | Task Name                 |");
     println!("===================================");
-
     if task_list.is_empty() {
         println!("|     | {0: <26}|", "");
     } else {
@@ -60,21 +66,29 @@ fn view_tasks(task_list: &Vec<String>) {
 }
 
 fn add_task(task_list: &mut Vec<String>) {
-    let mut new_task_name = String::new();
+    println!("Adding a New Task");
     loop {
+        let mut new_task_name = String::new();
+
         print!("Task Name: ");
         let _ = io::stdout().flush();
         io::stdin()
             .read_line(&mut new_task_name)
             .expect("Failed to read line!");
         new_task_name = new_task_name.trim().to_string();
-        if new_task_name.len() > 0 {
-            break;
-        } else {
+
+        if new_task_name.len() < 1 {
             println!("Can't be empty!");
+            continue;
         }
+        if new_task_name.len() >= 20 {
+            println!("Length of task name must be between 1 - 20!");
+            continue;
+        }
+
+        task_list.push(new_task_name.trim().to_string());
+        break;
     }
-    task_list.push(new_task_name.trim().to_string());
     println!("Update: Successfully added new task!");
 }
 
@@ -84,6 +98,7 @@ fn remove_task(task_list: &mut Vec<String>) {
         return;
     }
 
+    println!("Deleting a Task");
     let mut remove_task_index = String::new();
     view_tasks(task_list);
     println!("Which task would you like to remove?");
@@ -101,7 +116,7 @@ fn remove_task(task_list: &mut Vec<String>) {
             }
             task_list.remove(ind - 1);
             println!("Update: Successfully removed task!");
-        },
+        }
         Err(_) => {
             println!("Please enter a task number!");
         }
@@ -111,33 +126,25 @@ fn remove_task(task_list: &mut Vec<String>) {
 fn main() {
     let mut task_list: Vec<String> = Vec::new();
     loop {
-        println!("\nTodo List App");
+        clear_screen();
+        println!("Todo List App");
         println!("==============");
 
         let command: Command = handle_command_input();
+        clear_screen();
         match command {
-            Command::View => {
-                view_tasks(&task_list);
-            },
-            Command::Add => {
-                add_task(&mut task_list);
-            },
-            Command::Remove => {
-                remove_task(&mut task_list);
-            },
-            Command::Exit => {
-                break;
-            },
-            Command::Unknown => {
-                println!("Unknown command!");
-            },
+            Command::View => view_tasks(&task_list),
+            Command::Add => add_task(&mut task_list),
+            Command::Remove => remove_task(&mut task_list),
+            Command::Exit => break,
+            Command::Unknown => println!("Unknown command!"),
         };
-        
+
         println!("Press enter to continue...");
         let mut buffer = String::new();
         io::stdin()
             .read_line(&mut buffer)
             .expect("Failed to read line!");
         println!("\n\n\n\n");
-    };
+    }
 }
