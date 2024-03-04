e4c6ec1952c92847e5b8a67ded054c1e0b853c5f
Adrian <adrianhartanto0312@gmail.com>
2024-03-04 15:06:29 +0700

    Feat: add shorten link functionality

--- url_shortener/src-tauri/src/main.rs
+++ url_shortener/src-tauri/src/main.rs
@@ -1,9 +1,53 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use serde::Deserialize;
+use std::collections::HashMap;
+use tauri::async_runtime::block_on;
+
+#[derive(Debug, Deserialize)]
+struct LinkData {
+    tiny_url: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct LinkResponse {
+    code: u8,
+    data: LinkData,
+}
+
+async fn send_request(url: &str) -> Result<LinkResponse, reqwest::Error> {
+    let client = reqwest::Client::new();
+
+    // constructing request body
+    let mut request_body: HashMap<&str, &str> = HashMap::new();
+    request_body.insert("url", url);
+
+    let shortened_link: LinkResponse = client
+        .post("https://api.tinyurl.com/create")
+        .query(&[(
+            "api_token",
+            "85Ur3Dr54J9oVKIAOlpavRXuXvHblY5ChjiFWaD9x8mpgGFQGiqRWvBMqSmW",
+        )])
+        .json(&request_body)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(shortened_link)
+}
 
 #[tauri::command]
 fn shorten_url(url: &str) -> String {
-    format!("{}", url)
+    let link = send_request(&url);
+    match block_on(link) {
+        Ok(shortened_link) => match shortened_link.code {
+            0 => shortened_link.data.tiny_url,
+            _ => format!("Failed to shorten link!"),
+        },
+        Err(err) => format!("{}", err),
+    }
 }
 
 fn main() {
